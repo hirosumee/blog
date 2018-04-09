f775b5ba0cff94c47652692596a306745f46fd79
shentury <richardepope@my.smccd.edu>
2018-04-09 23:48:15 +0700

    auto load edited

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-var exec= require('child_process').exec;
-var http= require('http');
-var port=process.env.PORT||4000;
-var hexoProcess=exec('hexo server -p '+port);
-hexoProcess.stdout.on('data',(data)=>{
+var exec = require('child_process').exec;
+var http = require('http');
+var port = process.env.PORT || 4000;
+var hexoProcess = exec('hexo server -p ' + port);
+hexoProcess.stdout.on('data', (data) => {
     console.log(data.toString());
 });
-setInterval(()=>{
-    http.get('https://minblog.herokuapp.com');
-},300000);
+
+function autoload() {
+    setInterval(() => {
+        http.get('https://minblog.herokuapp.com', function () {
+            autoload();
+        });
+    }, 300000);
+}
+autoload();
