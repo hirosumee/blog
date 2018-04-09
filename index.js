fc82eacb6b7dbf0ad84cd03398bf17294373d694
shentury <richardepope@my.smccd.edu>
2018-04-09 19:45:48 +0700

    add auto access

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var exec= require('child_process').exec;
+var http= require('http');
 var port=process.env.PORT||4000;
 var hexoProcess=exec('hexo server -p '+port);
 hexoProcess.stdout.on('data',(data)=>{
     console.log(data.toString());
 });
+setInterval(()=>{
+    http.get('https://minblog.herokuapp.com');
+},300000);
