a24d7d16079955a73b25f77c360d098acf9a3293
shentury <richardepope@my.smccd.edu>
2018-04-10 01:33:37 +0700

    ok

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 var exec = require('child_process').exec;
-var http = require('http');
 var port = process.env.PORT || 4000;
 var hexoProcess = exec('hexo server -p ' + port);
 hexoProcess.stdout.on('data', (data) => {
     console.log(data.toString());
 });
-
-function autoload(http) {
-    setTimeout((http) => {
-        http.get('https://minblog.herokuapp.com', function () {
-            autoload();
-        });
-    }, 300000,http);
-}
-autoload(http);
